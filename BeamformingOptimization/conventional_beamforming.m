d766713c8a8d4996d1a42a370c3a51f43f7298f0
AmanSharma2006 <119773739+AmanSharma2006@users.noreply.github.com>
2025-04-20 00:36:21 +0530

    Add files via upload

---
+++ BeamformingOptimization/conventional_beamforming.m
@@ -0,0 +1,7 @@
+function snr_values = conventional_beamforming(features)
+    % Assume fixed/predefined beam directions
+    % You can replace this logic with any conventional algorithm (e.g., MRT)
+
+    % Simulate SNR values as the sum of features or other logic
+    snr_values = sum(features, 2);  % Example placeholder
+end
